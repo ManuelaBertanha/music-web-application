2b34e5fe67ae26bf025eaf5c82b9a548ec721756
ManuelaBertanha-FEUP <up202401489@edu.fe.up.pt>
2024-11-10 15:04:51 +0000

    Apagando comentário teste

--- music-web-application/Authorization/AccessToken.cs
+++ music-web-application/Authorization/AccessToken.cs
@@ -5,7 +5,7 @@ namespace music_web_application.Authorization
     public class AccessToken
     {
         [JsonPropertyName("access_token")]
-        private string Access_Token {  get; set; } // comentário teste!!!!
+        private string Access_Token {  get; set; }
 
         [JsonPropertyName("token_type")]
         private string Token_Type {  get; set; }
