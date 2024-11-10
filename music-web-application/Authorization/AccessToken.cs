6be66deeacf49c9f2a1669610d445296816385a2
ManuelaBertanha <bertanhamanuela@outlook.com>
2024-11-10 14:37:20 +0000

    Inclusão da classe AccessToken

---
+++ music-web-application/Authorization/AccessToken.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace music_web_application.Authorization
+{
+    public class AccessToken
+    {
+        [JsonPropertyName("access_token")]
+        private string Access_Token {  get; set; }
+
+        [JsonPropertyName("token_type")]
+        private string Token_Type {  get; set; }
+
+        [JsonPropertyName("expires_in")]
+        private int Expires_In { get; set; }
+    }
+}
