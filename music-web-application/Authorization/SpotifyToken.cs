16a150f5d820a039f45b766db4f2e1bca9834dc1
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-01-11 20:29:49 +0000

    Ajuste no nome das propriedades

--- music-web-application/Authorization/SpotifyToken.cs
+++ music-web-application/Authorization/SpotifyToken.cs
@@ -5,12 +5,12 @@ namespace music_web_application.Authorization
     public class SpotifyToken
     {
         [JsonPropertyName("access_token")]
-        private string AccessToken {  get; set; }
+        private string _accessToken {  get; set; }
 
         [JsonPropertyName("token_type")]
-        private string TokenType {  get; set; }
+        private string _tokenType {  get; set; }
 
         [JsonPropertyName("expires_in")]
-        private int ExpiresIn { get; set; }
+        private int _expiresIn { get; set; }
     }
 }
