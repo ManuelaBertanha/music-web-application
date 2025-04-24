a98116c43ed69ad8158bfcdf2517cc52ddff5bc0
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-04-24 16:13:55 +0100

    Inclusão de documentação XML para os endpoints de cada Controller.

--- music-web-application/Controller/SpotifyAuthorizationController.cs
+++ music-web-application/Controller/SpotifyAuthorizationController.cs
@@ -14,6 +14,12 @@ namespace music_web_application.Controller
             _spotifyAuthService = spotifyAuthService;
         }
         
+        /// <summary>Returns the access token.</summary>
+        /// <remarks>Returns the access token for a given Spotify account, allowing other Spotify API endpoints to be used.</remarks>
+        /// <response code="200">Access token obtained successfully.</response>
+        /// <response code="400">Malformed request because some parameter was passed incorrectly.</response>
+        /// <response code="401">Missing or invalid credentials in Header.</response>
+        /// <response code="500">Internal Server Error.</response>
         [HttpGet("getAccessToken")]
         public async Task<IActionResult> GetAccessToken()
         {
