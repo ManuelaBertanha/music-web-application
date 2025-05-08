0d79243eeecd371c3f46eeb89c3e77fc0d9af07f
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-05-08 18:19:11 +0100

    Classe de retorno padrão para os endpoints - em caso de sucesso.

---
+++ music-web-application/Model/StandardResponse.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace music_web_application.Model;
+
+public class StandardResponse<T>
+{
+    [JsonPropertyName("data")]
+    public T? DataResponse { get; set; }
+    public StandardResponse(T dataResponse)
+    {
+        DataResponse = dataResponse;
+    }
+}
