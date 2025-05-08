e3c515608a51024dec6c8a927981a75f61250ac2
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-05-08 18:28:15 +0100

    Utilização de 'IAlbumHandlingService', captura de exceções e melhorias na documentação XML.

--- music-web-application/Controllers/AlbumsController.cs
+++ music-web-application/Controllers/AlbumsController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using music_web_application.Services;
+using music_web_application.Model;
+using music_web_application.Model.Albums;
+using music_web_application.Model.Errors;
+using music_web_application.Services.Interfaces;
 
 namespace music_web_application.Controllers;
 
@@ -7,22 +10,40 @@ namespace music_web_application.Controllers;
 [Route("albums")]
 public class AlbumsController : ControllerBase
 {
-    private readonly AlbumHandlingService _albumHandlingService;
+    private readonly IAlbumHandlingService _albumHandlingService;
+    private readonly IWebHostEnvironment _env;
 
-    public AlbumsController(AlbumHandlingService albumHandlingService)
+    public AlbumsController(IAlbumHandlingService albumHandlingService, IWebHostEnvironment env)
     {
         _albumHandlingService = albumHandlingService;
+        _env = env;
     }
     
     /// <summary>Returns information for a single album.</summary>
     /// <remarks>Returns Spotify catalog information for a single album identified by albumId.</remarks>
     /// <response code="200">Album data obtained successfully.</response>
+    /// <response code="400">Bad Request.</response>
     /// <response code="401">Access token revoked by user or expired.</response>
     /// <response code="403">Bad OAuth request.</response>
     /// <response code="429">The app has exceeded its rate limits.</response>
+    /// <response code="500">Internal Server Error.</response>
     [HttpGet("getAlbum/{albumId}")]
-    public IActionResult GetAlbumById([FromRoute] string albumId)
+    public async Task<IActionResult> GetAlbumById([FromRoute] string albumId)
     {
-        return Ok();
+        try
+        {
+            var albumData = await _albumHandlingService.FetchAlbumDataById(albumId);
+            return Ok(new StandardResponse<Album>(albumData));
+        }
+        catch (WebAppException ex)
+        {
+            return StatusCode(ex.StatusCode ?? 500, StandardErrorResponse.CreateErrorResponse(ex, _env));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, StandardErrorResponse.CreateErrorResponse(ex, _env));
+            
+        }
     }
+    
 }
